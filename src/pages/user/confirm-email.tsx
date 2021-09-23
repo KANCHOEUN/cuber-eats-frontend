7352f4832cfab56365ebcc93e2db00be6900d5ea
KANCHOEUN <kancho1216@naver.com>
2021-09-23 16:44:17 +0000

    ✨ Add logic using writeFragment on ConfirmEmail

--- src/pages/user/confirm-email.tsx
+++ src/pages/user/confirm-email.tsx
@@ -1,6 +1,9 @@
-import { useMutation } from "@apollo/client";
+import { useApolloClient, useMutation } from "@apollo/client";
 import gql from "graphql-tag";
 import React, { useEffect } from "react";
+import { Helmet } from "react-helmet";
+import { useHistory } from "react-router";
+import { useMe } from "../../hooks/useMe";
 import { useQueryParam } from "../../hooks/useQueryParam";
 import {
   verifyEmail,
@@ -17,14 +20,38 @@ const VERIFY_EMAIL_MUTATION = gql`
 `;
 
 export const ConfirmEmail = () => {
+  const { data: userData } = useMe();
+  const client = useApolloClient();
+  const history = useHistory();
+
+  const onCompleted = (data: verifyEmail) => {
+    const { ok } = data.verifyEmail;
+    if (ok && userData?.me.id) {
+      client.writeFragment({
+        id: `User:${userData.me.id}`,
+        fragment: gql`
+          fragment VerifiedUser on User {
+            verified
+          }
+        `,
+        data: {
+          verified: true,
+        },
+      });
+
+      history.push("/");
+    }
+  };
+
   const [verifyEmail, { loading: verifyingEmail }] = useMutation<
     verifyEmail,
     verifyEmailVariables
-  >(VERIFY_EMAIL_MUTATION);
+  >(VERIFY_EMAIL_MUTATION, {
+    onCompleted,
+  });
   const code = useQueryParam("code") || "";
 
   useEffect(() => {
-    console.log(code);
     verifyEmail({
       variables: {
         input: {
@@ -36,6 +63,9 @@ export const ConfirmEmail = () => {
 
   return (
     <div className="mt-52 flex flex-col items-center justify-center">
+      <Helmet>
+        <title>Verify Email | Cuber Eats</title>
+      </Helmet>
       <h2 className="text-xl mb-2 font-semibold">Confirming Email...</h2>
       <h4 className="text-gray-600 text-base">
         Please wait, don't close this page.
